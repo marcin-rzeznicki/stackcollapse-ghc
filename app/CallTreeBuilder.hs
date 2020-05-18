a034d4edda9d5a0a8b2d4089558cdd129e1a7a84
Marcin Rzeźnicki <marcin.rzeznicki@gmail.com>
2020-05-18 15:22:13 +0200

    Add GPL header to source files

--- app/CallTreeBuilder.hs
+++ app/CallTreeBuilder.hs
@@ -1,3 +1,19 @@
+{-  stackcollapse-ghc - fold GHC prof files into flamegraph input
+    Copyright (C) 2020 Marcin Rzeźnicki
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -29,7 +45,8 @@ tryBuildCallForest format opMode nls =
   where
     builder = callTreeBuilder format opMode
 
-type CallTreeBuilder = FoldM (StateT BuilderState MayFail) NumberedRow CallForest
+type CallTreeBuilder =
+  FoldM (StateT BuilderState MayFail) NumberedRow CallForest
 
 type NumberedRow = (Int, Row)
 
--- app/Config.hs
+++ app/Config.hs
@@ -1,3 +1,19 @@
+{-  stackcollapse-ghc - fold GHC prof files into flamegraph input
+    Copyright (C) 2020 Marcin Rzeźnicki
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
@@ -52,7 +68,7 @@ data AnnotationMode = NoAnnotations
   deriving stock (Eq)
 
 prependUserModule :: StackCollapseConfig -> UserModule -> StackCollapseConfig
-prependUserModule config@Config { userModules } modu =
+prependUserModule config @ Config { userModules } modu =
   config { userModules = modu:userModules }
 
 
